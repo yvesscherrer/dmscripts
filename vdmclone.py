5eb5edc6f8ddbcfff28151be8dc6bd08a65c163a
Yves Scherrer <yves.scherrer@gmail.com>
2017-05-25 00:10:32 +0300

    fix geocorrel name

--- vdmclone.py
+++ vdmclone.py
@@ -584,7 +584,7 @@ def geoCorrelExperiment(simMatrixNames, geoMatrixName, classificationTuples):
 
 	for id in paramMatrices:
 		(finalMatrix, headers) = classify(paramMatrices[id], classificationTuples, allParamMatrices)
-		writeMatrix(finalMatrix, "{}-{}.csv".format(id), geoColumns, headers)
+		writeMatrix(finalMatrix, "{}-geocorrel.csv".format(id), geoColumns, headers)
 
 
 def clusterExperiment(simMatrixNames, clusterTuples):
