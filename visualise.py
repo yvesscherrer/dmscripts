507ab29a45535fa79a53b892b772b63b6520d447
yvesscherrer <yves.scherrer@gmail.com>
2017-05-25 19:14:00 +0300

    refactor

--- visualise.py
+++ visualise.py
@@ -1,46 +1,8 @@
 #!  python
 # -*- coding: iso-8859-1 -*-
 
-import arcpy, os
-
-arcpy.env.qualifiedFieldNames = False
-
-
-def createShpFile(datafile, shpmodelfile, outshp):
-	print "Create shp:", datafile, "=>", outshp
-	arcpy.MakeFeatureLayer_management(shpmodelfile, "temp_layer")
-	arcpy.AddJoin_management("temp_layer", "BFS", datafile, "LOC", join_type="KEEP_COMMON")
-	arcpy.CopyFeatures_management("temp_layer", outshp)
-	arcpy.DeleteField_management(outshp, ["ORIG_FID", "LOC_X", "LOC_Y"])
-	arcpy.Delete_management("temp_layer")
-	print "Done"
-
-
-def createVisualisation(shpfile, mxdfile, lyrfile, manualBreakValues, outlyr="", outpdf="", outpng=""):
-	print "Create png/pdf:", shpfile, "=>", outlyr, outpdf, outpng
-	mxd = arcpy.mapping.MapDocument(mxdfile)
-	df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-	shp = arcpy.mapping.Layer(shpfile)
-	lyr = arcpy.mapping.Layer(lyrfile)
-	arcpy.mapping.UpdateLayer(df, shp, lyr, True)
-
-	print shp.symbologyType
-	if manualBreakValues != []:
-		shp.symbology.classBreakValues = manualBreakValues
-	elif shp.symbologyType == "GRADUATED_COLORS":
-		shp.symbology.reclassify()
-
-	if outlyr != "":
-		shp.saveACopy(outlyr)
-	arcpy.mapping.AddLayer(df, shp)
-	if outpdf != "":
-		arcpy.mapping.ExportToPDF(mxd, outpdf)
-	if outpng != "":
-		arcpy.mapping.ExportToPNG(mxd, outpng, resolution=300)
-	del shp, df, mxd, lyr
-	print "Done"
-
-##############################################
+# this requires ArcGIS and can only be executed from Windows Python (the one installed with ArcGIS)
+import arcinterface, os
 
 basepath = "C:/Users/yvessche/switchdrive/dialectometry-stuff/sads-malaga/"
 csvpath = basepath + "csv/"
@@ -63,11 +25,11 @@ for filename in os.listdir(csvpath):
 	if "-mds" in filename:	# mds not supported yet
 		continue
 	shpname = filename.replace("-", "_").replace(".csv", ".shp")
-	createShpFile(csvpath + filename, shpmodel, shppath + shpname)
+	arcinterface.joinCsvWithShp(csvpath + filename, shpmodel, shppath + shpname, "LOC", "BFS", ["ORIG_FID", "LOC_X", "LOC_Y"])
 	pngname = shpname.replace(".shp", ".png")
 	if "-clu" in filename:		# categorical features
 		lyrfile = basepath + "rawshp/point_clu6.lyr"
-		createVisualisation(shppath + shpname, mxdfile, lyrfile, [], "", "", pngpath + pngname)
+		arcinterface.createVisualisation(shppath + shpname, mxdfile, lyrfile, [], "", "", pngpath + pngname)
 	else:
 		lyrfile = basepath + "rawshp/point_value_6jenks.lyr"
-		createVisualisation(shppath + shpname, mxdfile, lyrfile, [], "", "", pngpath + pngname)
+		arcinterface.createVisualisation(shppath + shpname, mxdfile, lyrfile, [], "", "", pngpath + pngname)
