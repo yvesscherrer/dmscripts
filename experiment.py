cd9148d9a988b79fca8b8b075c4102e1d37a708c
yvesscherrer <yves.scherrer@gmail.com>
2017-05-25 18:59:31 +0300

    bugfix

--- experiment.py
+++ experiment.py
@@ -10,23 +10,27 @@ vdmclone.outputdelimiter = ';'
 
 
 simm, headers = vdmclone.loadSimilarityMatrix("results34.ppl.4gr.csv")
+simm = vdmclone.invertMatrix(simm)	# ppl matrices are in fact distance matrices
 headers = [x.replace("ID", "") for x in headers]
 clum = vdmclone.cluster(simm, headers, "wm", 6)
 vdmclone.writeMatrix(clum, "results34.ppl.4gr.6wm.csv", rowHeaders=headers, columnHeaders=["CLU"])
 
 simm, headers = vdmclone.loadSimilarityMatrix("results34.kld.4gr.csv")
+simm = vdmclone.invertMatrix(simm)	# kld matrices are in fact distance matrices
 headers = [x.replace("ID", "") for x in headers]
-clum = cluster(simm, headers, "wa", 6)
+clum = vdmclone.cluster(simm, headers, "wa", 6)
 vdmclone.writeMatrix(clum, "results34.kld.4gr.6wa.csv", rowHeaders=headers, columnHeaders=["CLU"])
 
 simm, headers = vdmclone.loadSimilarityMatrix("results34.ppl.4gr.csv")
+simm = vdmclone.invertMatrix(simm)	# ppl matrices are in fact distance matrices
 headers = [x.replace("ID", "") for x in headers]
 for (algo, nclust) in (("wm", 6), ("wm", 10), ("wa", 6), ("wm", 10)):
-	clum = cluster(simm, headers, algo, nclust)
+	clum = vdmclone.cluster(simm, headers, algo, nclust)
 	vdmclone.writeMatrix(clum, "results34.ppl.4gr.{}{}.csv".format(nclust, algo), rowHeaders=headers, columnHeaders=["CLU"])
 
 simm, headers = vdmclone.loadSimilarityMatrix("results34.ppl.4gr.csv")
+simm = vdmclone.invertMatrix(simm)	# ppl matrices are in fact distance matrices
 headers = [x.replace("ID", "") for x in headers]
 for i in (1, 3):
-	mdsm = mds(simm, headers, i)
+	mdsm = vdmclone.mds(simm, headers, i)
 	vdmclone.writeMatrix(mdsm, "results34.ppl.4gr.mds{}.csv".format(i), rowHeaders=headers, columnHeaders=["MDS{}".format(x) for x in range(1, i+1)])
